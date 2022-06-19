e9218f1da8d4fd4d8e7fe1fe98d39aa54c6c2a34
Anatoliy <av.mikhaylenko@yandex.ru>
2022-06-20 01:08:51 +0300

    first victory

---
+++ ДЗ1/Program.cs
@@ -0,0 +1,16 @@
+﻿int a, b, max;
+
+Console.WriteLine("Введите число ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+{   
+    max = b;
+}
+else
+    max = a;
+    
+Console.Write("Максимальное число: " + max);
