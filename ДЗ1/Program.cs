767bb20fa6e73ec0faf11ec4f71000ac42d642c1
Anatoliy <av.mikhaylenko@yandex.ru>
2022-06-22 21:58:27 +0300

    add 4 task

--- ДЗ1/Program.cs
+++ ДЗ1/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 2
 
-int a, b, max, min;
+/*int a, b, max, min;
 
 Console.WriteLine("Введите число ");
 a = Convert.ToInt32(Console.ReadLine());
@@ -18,4 +18,35 @@ else
     min = b;
     
 Console.WriteLine("Максимальное число: " + max);
-Console.WriteLine("Минимальное число: " + min);
+Console.WriteLine("Минимальное число: " + min); */
+
+//Задача 4
+
+int a, b, c, max;
+
+Console.WriteLine("Введите число ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{   
+    max = a;
+}
+else
+    max = b;
+
+if (max > c)
+{  
+   Console.WriteLine("Максимальное число: " + max);
+}
+else
+if (max < c)
+{
+    max = c;
+    Console.WriteLine("Максимальное число: " + max);
+}
