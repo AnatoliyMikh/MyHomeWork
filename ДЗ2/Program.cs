3b0dfeff4e4cadf454db390afbecdd62dfc737dc
Anatoliy <av.mikhaylenko@yandex.ru>
2022-06-30 02:17:16 +0300

    error

--- ДЗ2/Program.cs
+++ ДЗ2/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int SecondDigit(int n1)
 {
     int receivednumber = n1%100;       
@@ -11,12 +11,12 @@ Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
  
 int res = SecondDigit(num);
-Console.WriteLine("Вторая цифра: " + res);
+Console.WriteLine("Вторая цифра: " + res);*/
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 void ThirdDigit(int num1)
 {
-    int num2 = num1/100;
+    /*int num2 = num1/100;
 
     if(num2 <= 99)
         {
@@ -27,14 +27,34 @@ void ThirdDigit(int num1)
         int number = num2 % 10;
 
         Console.WriteLine("Третья цифра " + number);
-        }    
+        }*/
+    if (num1 <= 99)
+    {
+        Console.WriteLine("У числа нет третьей цифры");
+    }
+    else
+    {
+        do
+        {
+            num1 = num1/10;
+        }   
+        while(num1 > 100 && num1 <= 999);
+        if (num1 < 100)
+        {
+            Console.WriteLine("loshara");
+        }
+
+        int num2 = num1%10;
+        Console.WriteLine("Третья цифра " + num2);
+    }
 }
 
-Console.WriteLine("Введите трехзначное число: ");
+Console.WriteLine("Введите число: ");
 int chislo = Convert.ToInt32(Console.ReadLine());
  
 ThirdDigit(chislo);
 
+/*
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 void WeekEnd(int d1)
 {
@@ -48,4 +68,4 @@ void WeekEnd(int d1)
 Console.WriteLine("Введите число: ");
 int day = Convert.ToInt32(Console.ReadLine());
  
-WeekEnd(day);
+WeekEnd(day);*/
