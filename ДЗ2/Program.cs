16eed3c175858ae08e92396adcc608a9a56b3bb1
Anatoliy <av.mikhaylenko@yandex.ru>
2022-06-30 01:00:39 +0300

    add task 15

--- ДЗ2/Program.cs
+++ ДЗ2/Program.cs
@@ -1,13 +1,11 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-/*
+
 int SecondDigit(int n1)
 {
     int receivednumber = n1%100;       
     int result = receivednumber/10;
     return result;
 }
-
- 
 
 Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
@@ -35,5 +33,19 @@ void ThirdDigit(int num1)
 Console.WriteLine("Введите трехзначное число: ");
 int chislo = Convert.ToInt32(Console.ReadLine());
  
-ThirdDigit(chislo);*/
+ThirdDigit(chislo);
 
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+void WeekEnd(int d1)
+{
+    if(d1 ==6 && d1 == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+        Console.WriteLine("Будний день");
+}
+Console.WriteLine("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+ 
+WeekEnd(day);
