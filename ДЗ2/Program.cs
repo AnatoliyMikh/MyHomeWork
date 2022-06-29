13d2b0accec402e64aa5b943f54286c7bd58f486
Anatoliy <av.mikhaylenko@yandex.ru>
2022-06-29 22:23:59 +0300

    add title task 13

--- ДЗ2/Program.cs
+++ ДЗ2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SecondDigit(int n1)
+{
+    int receivednumber = n1%100;       
+    int result = receivednumber/10;
+    return result;
+}
+ 
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ 
+int res = SecondDigit(num);
+Console.WriteLine("Вторая цифра: " + res);
