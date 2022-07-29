0b19a904daad913b18bc665649d6fde0a1a4345e
Anatoliy <av.mikhaylenko@yandex.ru>
2022-07-29 19:54:33 +0300

    Finish task 43

--- ДЗ6/Program.cs
+++ ДЗ6/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
 double[] CreateNewArray(int n1)
 {
    double[] arr = new double[n1];
@@ -28,7 +29,38 @@ int n1 = Convert.ToInt32(Console.ReadLine());
 
 double[] array = CreateNewArray(n1);
 Console.WriteLine("Количество элементов равно: " + FindElement(array));
-
+*/
 
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+double FindKoord1(double k1, double b1, double k2, double b2)
+{
+    double x = 0;
+
+    x = (b1-b2)/(k2-k1);    
+
+    return x;
+}
+
+double FindKoord2(double k1, double b1, double k2, double b2)
+{
+    double y = 0;
+
+    y = (k2*(b1-b2))/(k2-k1);    
+
+    return y;
+}
+
+Console.WriteLine("Введите k1: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите b1: ");
+double m1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите k2: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите b2: ");
+double m2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Пересечение прямых в координатах " + (FindKoord1(n1, m1, n2, m2) + ";" +FindKoord2(n1, m1, n2, m2)));
