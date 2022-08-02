f49af828535cad4960c800dfef0772fecd333334
Anatoliy <av.mikhaylenko@yandex.ru>
2022-08-02 19:23:31 +0300

    finish task 52

--- ДЗ7/Program.cs
+++ ДЗ7/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+/*
 double[,] CreateRandomArray(int rows, int columns)
 {
     double[,] newArray = new double[rows,columns];
@@ -14,7 +15,9 @@ void ShowArray(double[,] array)
     for(int i=0; i < array.GetLength(0); i++)
     {
         for(int j=0; j < array.GetLength(1); j++)
+        {
             Console.Write(array[i,j] + " ");
+        }
         Console.WriteLine();
     }
     Console.WriteLine();
@@ -26,9 +29,57 @@ Console.Write("Введите количество столбцов: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 double[,] myArray = CreateRandomArray(m,n);
-ShowArray(myArray);
+ShowArray(myArray); */
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int[,] CreateRandomArray(int rows, int columns)
+{
+    int[,] newArray = new int[rows,columns];
+
+    for(int i=0; i < rows; i++)
+        for(int j=0; j < columns; j++)
+            newArray[i,j] = new Random().Next(0, 100);
+    return newArray;
+}
+
+void ShowArray(int[,] array)
+{
+    for(int i=0; i < array.GetLength(0); i++)
+    {
+        for(int j=0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FindАverage(int[,] arr)
+{
+    for(int i=0; i < arr.GetLength(0); i++)
+    {
+        int result = 0;
+
+        for(int j=0; j < arr.GetLength(1); j++)
+        {
+            result = result+ arr[i,j];
+        }
+        result = result / arr.GetLength(1);
+
+        Console.WriteLine(result);
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandomArray(m,n);
+ShowArray(myArray);
+FindАverage(myArray);
