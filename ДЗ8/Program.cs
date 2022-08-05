c257dda9a3b88052ce34d3d9033d32cbb0a99255
Anatoliy <av.mikhaylenko@yandex.ru>
2022-08-05 16:02:34 +0300

    Finish task 62

--- ДЗ8/Program.cs
+++ ДЗ8/Program.cs
@@ -57,7 +57,7 @@ Console.WriteLine(Ordering(myArray));
 */
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-
+/*
 int[,] CreateRandomArray(int rows, int columns)
 {
     int[,] newArray = new int[rows,columns];
@@ -129,6 +129,70 @@ ShowArray(myArray);
 
 FindString(myArray);
 
-
+*/
 
 // Задача 62. Заполните спирально массив 4 на 4.
+int[,] Spiral(int n, int b)
+{
+    int[,] result = new int[n, b];
+    int columns = result.GetLength(1);
+    int rows = result.GetLength(0);
+    int value = 1;
+    for (int i = 0; value <= n * b; i++)
+    {
+        if (value <= n * b)
+        {
+            for (int j = i; j < columns - i; j++)
+            {
+                result[i, j] = value;
+                value++;
+            }
+        }
+        if (value <= n * b)
+        {
+            for (int k = i + 1; k < rows - i; k++)
+            {
+                result[k, columns - (1 + i)] = value;
+                value++;
+            }
+        }
+        if (value <= n * b)
+        {
+            for (int m = columns - (2 + i); m >= i; m--)
+            {
+                result[rows - (1 + i), m] = value;
+                value++;
+            }
+        }
+        if (value <= n * b)
+        {
+            for (int s = rows - (2 + i); s >= i + 1; s--)
+            {
+                result[s, i] = value;
+                value++;
+            }
+        }
+    }
+    return result;
+}
+
+void Show2dArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rows = 4;
+
+int columns = 4;
+
+
+int[,] myArr = Spiral(rows, columns);
+
+Show2dArray(myArr);
