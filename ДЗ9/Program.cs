6b1da671183e5681664c504afea37dad9b832f56
Anatoliy <av.mikhaylenko@yandex.ru>
2022-08-08 20:51:55 +0300

    start task 66

--- ДЗ9/Program.cs
+++ ДЗ9/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Задача 64: Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+/*
 int Amount(int i, int n)
 {   
     if(n > 10) 
@@ -15,6 +16,23 @@ int N = Convert.ToInt32(Console.ReadLine());
 
 int m = 1;
 
-Console.WriteLine("Количество цифр в числе " + Amount(m, N));
+Console.WriteLine("Количество цифр в числе " + Amount(m, N)); */
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+void SumOfDigits(int m, int n)
+{
+    if (m <= n)
+        if(m > 0 && m % 10 ==0)
+            {
+               m = m + SumOfDigits(m+1, n);
+            }
+    Console.WriteLine(m);
+}
+
+Console.WriteLine("Введите начальное число отрезка: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите конечное число отрезка: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Writeline("Сумма натуральных чисел в отрезке равна: " + SumOfDigits(M, N));
